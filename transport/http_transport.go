ab00c17ce943c68daefec06b4e1dcd3e1ffd55dd
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2016-05-25 12:24:36 +0100

    Transport: more logs

--- transport/http_transport.go
+++ transport/http_transport.go
@@ -166,6 +166,7 @@ func (h *httpTransportClient) Recv(m *Message) error {
 	}
 
 	hrrs := metrics.NewTiming()
+	startTime := time.Now()
 	rsp, err := http.ReadResponse(h.buff, r)
 	if err != nil {
 		return err
@@ -182,7 +183,17 @@ func (h *httpTransportClient) Recv(m *Message) error {
 	hras.Send("micro.transport.http.recv.readall.time")
 	metrics.Gauge("micro.transport.http.recv.readall.length", len(b))
 
-	log.Printf("Transport client slow reads: %s", string(b))
+	log.Printf("Transport client slow reads(%d) body: %s", len(b), string(b))
+
+	if time.Now().Sub(startTime).Seconds() > float64(2) {
+		log.Printf("transport client slow reads status: %d", rsp.StatusCode)
+		bb, berr := ioutil.ReadAll(h.buff)
+		if berr != nil {
+			log.Printf("Transport client slow reads error: %s", berr.Error())
+		} else {
+			log.Printf("Transport client slow reads valid: %s", string(bb))
+		}
+	}
 
 	if rsp.StatusCode != 200 {
 		return errors.New(rsp.Status + ": " + string(b))
